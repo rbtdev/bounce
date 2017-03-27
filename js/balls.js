fcdc844b63dbd733a50cf3731cd6a0eeb152d9fc
Rob Thuleen <rob.thuleen@gmail.com>
2017-03-26 21:22:55 -0700

    Added start button and ball count textbox

--- js/balls.js
+++ js/balls.js
@@ -1,14 +1,13 @@
 function Circle(opts) {
     var radius = opts.radius || 100;
-    var x = opts.x
-    var y = opts.y
+    var x = opts.x || 0;
+    var y = opts.y || 0;
     this.radius = radius
     this.x = x;
     this.y = y;
-    this.vx = opts.vx
-
-    console.log("Vx = " + this.vx)
+    this.vx = opts.vx || 0;
     this.vy = opts.vy || 0;
+    this.bounce = opts.bounce || 1;
     this.g = .06
     this.lastTime = Date.now();
     var c = $('<div>');
@@ -21,10 +20,10 @@ function Circle(opts) {
     c.css('top', this.y - this.radius);
     c.css('left', this.x - this.radius)
     c.on('click', function () {
-        this.vy = -1
+        this.vy = -3
     }.bind(this))
-    this.c = c;
-    $("body").append(this.c);
+    this.shape = c;
+    $("body").append(this.shape);
 
 }
 
@@ -35,57 +34,59 @@ Circle.prototype.move = function (dt) {
     this.x += this.vx * dt;
 
     if (this.y + this.radius + 2 > window.innerHeight) {
-        this.vy = -this.vy * .7
+        this.vy = -this.vy * this.bounce;
         this.y = window.innerHeight - this.radius - 2;
     }
     if (this.x < this.radius + 2) {
-        this.vx = -this.vx
+        this.vx = -this.vx * this.bounce;
         this.x = this.radius + 2
     } else if (this.x > window.innerWidth - this.radius - 2) {
-        this.vx = -this.vx;
+        this.vx = -this.vx * this.bounce;
         this.x = window.innerWidth - this.radius - 2;
     }
 
-    this.c.css('top', this.y - this.radius);
-    this.c.css('left', this.x - this.radius);
+    this.shape.css('top', this.y - this.radius);
+    this.shape.css('left', this.x - this.radius);
 
 }
 
 
-
-
 $(document).ready(function () {
-    var balls = [];
-    for (var i = 0; i < 10; i++) {
-        balls.push(new Circle({
-            radius: Math.random() * 25 + 12,
-            x: Math.random() * 1024 + 100,
-            y: 0,
-            vx: Math.random() * 2 - 1,
-            vy: Math.random() * 2 - 1
-        }));
-    }
-    frames = 0;
+    $("#start").on('click', function () {
+        var count = parseInt($("#count")[0].value);
+        var balls = [];
+        for (var i = 0; i < count; i++) {
+            balls.push(new Circle({
+                radius: Math.random() * 25 + 12,
+                x: Math.random() * 1024 + 100,
+                y: 0,
+                vx: Math.random() * 2 - 1,
+                vy: Math.random() * 2 - 1,
+                bounce: Math.random()
+            }));
+        }
 
-    var lastTime = null;
-    var start = Date.now();
-    window.requestAnimationFrame(drawFrame);
-
-
-    function drawFrame(now) {
-        lastTime = lastTime || now;
-        var dt = (now - lastTime);
-        lastTime = now;
-        $("#delta").text("DT:" + dt)
-        $.each(balls, function (index, ball) {
-            ball.move(dt);
-        })
-
-        frames++;
-        var delta = Date.now() - start
-        fps = frames / (delta / 1000);
-        $("#fps").text("FPS:" + fps.toFixed(2));
-        $("#time").text("TIME:" + delta / 1000)
+        frames = 0;
+        var lastTime = null;
+        var start = Date.now();
         window.requestAnimationFrame(drawFrame);
-    }
+
+        function drawFrame(now) {
+            lastTime = lastTime || now;
+            var dt = (now - lastTime);
+            lastTime = now;
+            $("#delta").text("DT:" + dt)
+            $.each(balls, function (index, ball) {
+                ball.move(dt);
+            })
+
+            frames++;
+            var delta = Date.now() - start
+            fps = frames / (delta / 1000);
+            $("#fps").text("FPS:" + fps.toFixed(2));
+            $("#time").text("TIME:" + delta / 1000)
+            window.requestAnimationFrame(drawFrame);
+        }
+    })
+
 })
